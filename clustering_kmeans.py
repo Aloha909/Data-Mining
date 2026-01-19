bb2a930b2411c1b7af255ac9d10b1c0617e08e1f
Raphaël <letourneur.raphael@gmail.com>
2026-01-19 08:27:27 +0100

    Organisation en 1 fichier = 1 méthode et un fichier affichage

--- clustering_kmeans.py
+++ clustering_kmeans.py
@@ -1,25 +1,9 @@
-
-
-
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.discriminant_analysis import StandardScaler
 
-def kmeans():
+def kmeans(df, scaled_data_df, k):
     print("RUNNING KMEANS")
-
-    df = pd.read_csv(
-        "./data_clean.csv"
-    )
-    df_sc = df[["lat","long"]]
-
-    scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(df_sc)
-
-    scaled_data_df = pd.DataFrame(data=scaled_data, columns=df_sc.columns)
-    scaled_data_df.head()
-
-    k = 100
 
     kmeans = KMeans(n_clusters=k, init='k-means++')
 
---
+++ methode.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Methode(Enum):
+    KMEANS = 1,
+    AGGLO = 2,
+    DBSCAN = 3, 
