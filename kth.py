debb07fd3e31709499fb14e769d40f95d12ca37c
robin <robin.renous83@gmail.com>
2026-02-01 14:40:14 +0100

    distance kth

--- kth.py
+++ kth.py
@@ -12,27 +12,37 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pydeck as pdk
 
-df = pd.read_csv(
-    "./data_clean.csv"
-)
-df["date_taken"] = pd.to_datetime(df["date_taken"], errors="coerce")
-df_sc = df[["lat","long"]]
 
-data_df = pd.DataFrame(data=df_sc, columns=df_sc.columns)
+df = pd.read_csv("./data_clean.csv")
 
-k = 3  
-X = data_df.sample(5000, random_state=42).to_numpy()
+k = 3
 
-nn = NearestNeighbors(n_neighbors=k)
-nn.fit(X)
 
-distances, _ = nn.kneighbors(X)
-k_distances = np.sort(distances[:, k-1])
+X_deg = df[["lat", "long"]].sample(5000, random_state=42).to_numpy()
 
+# degrés -> radians 
+X_rad = np.radians(X_deg)
+
+nn = NearestNeighbors(n_neighbors=k, metric="haversine")
+nn.fit(X_rad)
+
+distances, _ = nn.kneighbors(X_rad)
+
+k_dist_rad = np.sort(distances[:, k - 1])
+
+# radians -> mètres
+EARTH_RADIUS_M = 6371000.0
+k_dist_m = k_dist_rad * EARTH_RADIUS_M
+
+# Plot
 fig, ax = plt.subplots(figsize=(8, 4))
-ax.plot(k_distances)
+ax.plot(k_dist_m)
+
+
+ax.set_ylim(0, 200)
+
 ax.set_title(f"k-distance plot (k = {k})")
 ax.set_xlabel("Points sorted")
-ax.set_ylabel("Distance to k-th nearest neighbor")
+ax.set_ylabel("Distance to k-th nearest neighbor (meters)")
 
 st.pyplot(fig)
